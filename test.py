b99059065963073a5ddd10ed447871c1800b81c6
Hung Nguyen <hung135@hotmail.com>
2018-03-05 15:13:20 -0500

    adding testing for db_connection

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 import unittest
 
-from crypto import Coin, Exchange,Rule
+import db_utils
+
 
 class TestDBConn(unittest.TestCase):
     def test_init(self):
-        print("Testing Began")
+        print("Testing Began DB connection PostGres:")
+        db = db_utils.Connection(host='localhost',userid='postgres', dbschema='stg', dbtype='POSTGRES')
+        print(db.query('select 1'))
 
 
         print("Testing End")
