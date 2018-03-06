8e048fd7cc8493385ccbfc61cf3d6cd9d2e9d149
Hung Nguyen <hung135@hotmail.com>
2018-03-05 21:56:35 -0500

    added MYSQL adapter

--- test.py
+++ test.py
@@ -14,10 +14,10 @@ class Test_db_utils_postgres(unittest.TestCase):
     host = 'localhost'
     #host = '192.168.99.100'
     database = 'postgres'
-    userid = 'tester'
+    userid = 'postgres'
     dbtype = 'POSTGRES'
     schema = 'logging'
-    password = 'test'
+    password = ''
     port = 5432
     db = db_utils.Connection(host=host,
                              userid=userid,
