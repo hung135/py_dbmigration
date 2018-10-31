3903ee308bf80b4952ce4c879077a540fbe6c4d4
Hung Nguyen <hung.nguyen@cfpb.gov>
2018-10-31 16:01:59 +0000

    updated test.py

--- cloud9.sh
+++ cloud9.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-python3 -m virtualenv .env
-source .env/bin/activate
-pip install -r requirements.txt
+#python3 -m virtualenv .env
+#source .env/bin/activate
+#pip install -r requirements.txt
+sudo pip install -r requirements.txt
 docker pull postgres
 docker stop docker-postgres || true && docker rm docker-postgres || true
 docker run --name docker-postgres -p 5432:5432 -e POSTGRES_PASSWORD=docker -d postgres
