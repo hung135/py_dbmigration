05705638d83a09dd743a7516d59f0cafe923b999
Hung Nguyen <hung.nguyen@cfpb.gov>
2018-10-31 15:30:07 +0000

    update cloud9 script

--- cloud9.sh
+++ cloud9.sh
@@ -4,7 +4,8 @@ python3 -m virtualenv .env
 source .env/bin/activate
 pip install -r requirements.txt
 docker pull postgres
+docker stop docker-postgres || true && docker rm docker-postgres || true
 docker run --name docker-postgres -p 5432:5432 -e POSTGRES_PASSWORD=docker -d postgres
+#docker run -it  --rm -e PGPASSWORD=docker --link docker-postgres:postgres postgres psql -h postgres -U postgres
+
  
-# This line below will log you in to psql
-#docker run -it --rm --link docker-postgres:postgres postgres psql -h postgres -U postgres
