27ca898d404309b411bec291954ad69d0781d7c7
Hung Nguyen <hung.nguyen@cfpb.gov>
2018-11-02 15:20:04 +0000

    adding https nginx proxy

--- cloud9.sh
+++ cloud9.sh
@@ -16,28 +16,50 @@ docker run --name docker-postgres -p 5432:5432 -e POSTGRES_PASSWORD=docker -d po
 /etc/pki/tls/certs/make-dummy-cert dummy_cert.txt
 docker stop docker-pgadmin4 || true && docker rm docker-pgadmin4 -f || true
 docker pull dpage/pgadmin4
-docker run \
--p 8080:80 \
---name docker-pgadmin4 \
--e "PGADMIN_DEFAULT_EMAIL=admin" \
--e "PGADMIN_DEFAULT_PASSWORD=docker" \
--e "DEFAULT_USER=postgres" \
---link docker-postgres:postgres \
--d dpage/pgadmin4
+# docker run \
+# -p 1111:80 \
+# --name docker-pgadmin4 \
+# -e "PGADMIN_DEFAULT_EMAIL=admin" \
+# -e "PGADMIN_DEFAULT_PASSWORD=docker" \
+# -e "DEFAULT_USER=postgres" \
+# --link docker-postgres:postgres \
+# -d dpage/pgadmin4
 
 #documentation
 #https://docs.c9.io/docs/run-an-application
-#head -n 29 dummy_cert.txt>certificate.key
-#tail -n 26 dummy_cert.txt>certificate.cert
-# docker run -p 8080:443 \
-# --name docker-pgadmin4 \
-# -v "/private/var/lib/pgadmin:/var/lib/pgadmin" \
-# -v "$(pwd)/certificate.cert:/certs/server.cert" \
-# -v "$(pwd)/certificate.key:/certs/server.key" \
-# -e "PGADMIN_DEFAULT_EMAIL=postgres" \
-# -e "PGADMIN_DEFAULT_PASSWORD=docker" \
-# -e "PGADMIN_ENABLE_TLS=True" \
-# -d dpage/pgadmin4
+head -n 29 dummy_cert.txt>certificate.key
+tail -n 26 dummy_cert.txt>certificate.cert
+docker run -p 1111:443 \
+--name docker-pgadmin4 \
+-v "/private/var/lib/pgadmin:/var/lib/pgadmin" \
+-v "$(pwd)/certificate.cert:/certs/server.cert" \
+-v "$(pwd)/certificate.key:/certs/server.key" \
+-e "PGADMIN_DEFAULT_EMAIL=admin" \
+-e "PGADMIN_DEFAULT_PASSWORD=docker" \
+-e "PGADMIN_ENABLE_TLS=True" \
+-d dpage/pgadmin4
+sudo yum install nginx -y
+sudo mkdir /etc/nginx/sites-available
+sudo mkdir /etc/nginx/sites-enabled
+sudo echo "location / {
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_ssl_verify off;
+    proxy_pass http://localhost:8888;
+}" > /etc/nginx/sites-available/localhost.com
+cat >  localhost.com << EOL
+location / {
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_ssl_verify off;
+    proxy_pass http://localhost:1111;
+}
+EOL
 
 echo https://$(curl http://169.254.169.254/latest/meta-data/instance-id).vfs.cloud9.us-east-2.amazonaws.com >index.html
 echo https://$(curl http://169.254.169.254/latest/meta-data/public-ipv4) >>index.html
+
+cat localhost.com
+sudo cp localhost.com /etc/nginx/sites-available/localhost.com
+sudo ln -s /etc/nginx/sites-available/localhost.com /etc/nginx/sites-enabled/localhost.com
+sudo service nginx restart
