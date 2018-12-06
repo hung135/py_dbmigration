12c1c14074ddc3d61b41a3fb25ce5aaf1f28d085
Hung <hung.nguyen@cfpb.gov>
2018-12-06 12:27:51 -0500

    custom logic to abort import if newer data file exists

--- scrape_utils/scrape_util.py
+++ scrape_utils/scrape_util.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from lxml import html
 
 import requests
 from bs4 import BeautifulSoup
@@ -67,10 +68,15 @@ class Scrape:
     data_text = None
     data_soup = None
     session = None
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-    def __init__(self, endpoint, dataformat=None, username=None, password=None):
+ 
+
+    def __init__(self, endpoint, dataformat='json', login_payload=None,steps=[]):
         r = requests.get(endpoint)
         self.url = endpoint
+        self.login_payload=login_payload
+        self.steps=steps
         if dataformat is None or dataformat == 'json':
             self.data_json = json.loads(r.text)
         if dataformat == 'text':
@@ -78,22 +84,57 @@ class Scrape:
             self.data_json = json.dumps(r.text)
             self.data_soup = BeautifulSoup(r.text, "html.parser")
 
+        self.steps=steps
+        #self.login()
+        self.login_selenium()
+
     def printx(self):
 
         print(self.data_text)
 
         self.login()
-
-    def login(self, password=None):
-        payload = {'username': 'hung135@hotmail.com', 'password': password}
+    def logout(self):
         self.session = requests.session()
+    def login_selenium(self):
+        from selenium import webdriver
 
-        r = self.session.post(self.url, data=payload)
 
-        payload2 = {'accept': 'Yes', 'action': 'acceptTandC', 'acceptSubmit': 'Continue'}
-        r2 = self.session.post("https://freddiemac.embs.com/FLoan/Data/download.php", data=payload2)
+        browser = webdriver.Chrome() #replace with .Firefox(), or with the browser of your choice
+        
+        browser.get(self.url) #navigate to the page
+        username = browser.find_element_by_id("username") #username form field
+        password = browser.find_element_by_id("password") #password form field
 
-        print(r2.text)
+        username.send_keys(self.payload['username'])
+        password.send_keys(self.payload['password'])
+
+        submitButton = browser.find_element_by_id("loginSubmit") 
+        button.click() 
+        browser.get("http://example.com/page.php") #navigate to page behind login
+        innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a string
+
+    def login(self):
+        self.session = requests.session()
+        #payload = {'username': self.username, 'password': self.password}
+        r = self.session.post(self.url, data=self.login_payload)
+        print("Logged in Response:",r)
+        #print(r.text)
+    def print_steps(self,steps=None):
+        import urllib
+
+        steps_to_print=list(self.steps)
+        if steps is not None and len(steps_to_print)==0:
+            self.steps=steps
+            steps_to_print=steps
+
+        for url in steps_to_print:
+            print("Scraping: ",url)
+            r=self.session.get(url,headers=self.headers)
+            print(r.content)
+            #webpage = html.fromstring(r.content)
+            #z=webpage.xpath('//a/@href')
+            #self.print_steps(z)
+    
 
     def getJson(self):
 
