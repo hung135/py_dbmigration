549687bd1f7fa0a52d1ab6b68caaad30cc9cef39
hnguyen <hung135@hotmail.com>
2019-05-28 00:35:29 +0000

    fpds scrape test

---
+++ tests/config_parent.py
@@ -0,0 +1,5 @@
+class Config(object):
+    dirs = {
+        'data_dir': "/workspace/_sample_data/",
+        'working_dir': "/workspace/_sample_working_dir/",
+        'zip_data_dir': "/workspace/_sample_zip_data/"}
--- tests/test_api_fetch.py
+++ tests/test_api_fetch.py
@@ -10,6 +10,7 @@ import pprint
 from py_dbutils.rdbms import postgres as db_utils
 import requests 
 import json, yaml
+from config_parent import Config
 
 logging = log.getLogger()
 logging.setLevel(log.ERROR)
@@ -17,26 +18,8 @@ logging.setLevel(log.ERROR)
 TEST_SCHEMA = 'test'
 LOGGING_SCHEMA = 'logging'
 PROJECT_NAME= 'test_project'
-class Test_api_fetch(unittest.TestCase):
-    HOST = os.environ['PGHOST']
-    DATABASE = os.environ['PGDATABASE']
-    USERID = 'docker'
-    DATA_SCHEMA = 'prey'
-    DBPASSWORD = 'docker'
-    DBPORT = 5432
-      
-
-    db = db_utils.DB(host=HOST, userid=USERID, dbname=DATABASE, schema=DATA_SCHEMA,
-                                    pwd=DBPASSWORD,  port=DBPORT)
-    db.execute("create schema {}".format(LOGGING_SCHEMA))
-    db.execute("drop schema {} cascade".format(TEST_SCHEMA))
-    db.execute("create schema {}".format(TEST_SCHEMA))
-    db.execute("create schema {}".format(DATA_SCHEMA))
-    dirs = {
-        'sample_data_dir': "/workspace/_sample_data/",
-        'sample_working_dir': "/workspace/_sample_working_dir/",
-        'sample_zip_data_dir': "/workspace/_sample_zip_data/"}
-    data=None
+class Test_api_fetch(unittest.TestCase,Config):
+    
     vw_file='/workspace/tests/sql/logging_tables.sql'
     login_url="""https://www.fpds.gov/common/jsp/login.jsp"""
     user_id_field="USERNAME"
@@ -47,13 +30,24 @@ class Test_api_fetch(unittest.TestCase):
     api_url="""https://www.fpds.gov/fpdsng_cms/"""
     search_url="""https://www.fpds.gov/ezsearch/search.do?q"""
     search_pg1="https://www.fpds.gov/common/jsp/DocumentSearch.jsp?advancedResultsPage=Y&searchType=advanced&requestFromMainPage=N&pageNumber=1&ContractSelectionType=%20A&ContractSelectionType=A&actionSource=searchCriteriaPage&user=&agency_code=&award_piid=&vendor=GREENZONE+SOLUTIONS%2C+INC.&naics_id=&officeID=&officeName=&idvdAgencyAward_id=&signedFromDate=01/01/2018&signedToDate=12/31/2018&modifiedFromDate=&modifiedToDate=&actionCode=&fundingOfficeID=&fundingAgencyID=&obligatedAmountFrom=&obligatedAmountTo=&MINAMOUNT=&MAXAMOUNT=&modNumber=&STATUS=F&undefined=undefined"
-    def test_fetch_meta_api(self):
-        pay_load={self.user_id_field:os.environ["FPDS_USERID"],self.pwd_field:os.environ["FPDS_PWD"]}
-        with requests.Session() as session:
-            post = session.post(self.login_url,data=pay_load)
-            r = session.get(self.search_pg1)
-            print(r.text)
-
+    test_session_data="bbbbb"
+    
+    print(test_session_data)
+    def test_1fetch_meta_api(self):
+        #pull on once so we don't have to keep pulling
+        if not os.path.exists(os.path.join(self.dirs['working_dir'],'page_1')):
+            pay_load={self.user_id_field:os.environ["FPDS_USERID"],self.pwd_field:os.environ["FPDS_PWD"]}
+            with requests.Session() as session:
+                _ = session.post(self.login_url,data=pay_load)
+                r = session.get(self.search_pg1)  
+            #print(self.test_session_data)
+                with open(os.path.join(self.dirs['working_dir'],'page_1')) as f:
+                    f.write(r.text)
+             
+            print("test1")
+    def test_2process_session_data(self):
+        print(self.test_session_data)
+        print("test2")
         #test="""https://www.fpds.gov/ezsearch/search.do?q=greenzone+CONTRACTING_AGENCY_NAME%3A%22BUREAU+OF+THE+FISCAL+SERVICE%22+VENDOR_FULL_NAME%3A%22GREENZONE+SOLUTIONS%2C+INC.%22&s=FPDS.GOV&templateName=1.5.1&indexName=awardfull"""
         
         
