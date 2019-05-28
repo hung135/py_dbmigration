28ec96800892f2758f54322ab4e69ef53381acaa
Hung Nguyen <hung135@hotmail.com>
2019-05-28 02:11:29 +0000

    saving work

--- tests/test_data_load_pg_bulkcopy.py
+++ tests/test_data_load_pg_bulkcopy.py
@@ -15,21 +15,7 @@ logging.setLevel(log.DEBUG)
  
 class Test_db_utils_postgres(unittest.TestCase,Config):
   
-    DBPORT = 5432
-    SAMPLE_DATA_LINE_COUNT = 1500
-    SAMPLE_DATA_TOTAL_TABLES = 8  # None will get all tables
-    CLEAN_PREV = False
-    GENERATE_SAMPLE_DATA = False
-    GENERATE_SAMPLE_DATA_W_HEADER = True
-
-    SAMPLE_DATA_HAS_HEADER = False
-    GENERATE_CRC = False
-    GENERATE_FILE_ID = False
-    LIMIT_ROWS = None
-    START_ROW = 2
-    TRUNCATE_TABLE = True
- 
-  
+      
     
     def test_data_load(self):
         db=self.get_pg_database()
