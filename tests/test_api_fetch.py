124fff818ca39532c9f3fdad45259c8780a5a6f2
hnguyen <hung135@hotmail.com>
2019-05-28 01:06:09 +0000

    saving work

--- tests/test_api_fetch.py
+++ tests/test_api_fetch.py
@@ -11,6 +11,8 @@ from py_dbutils.rdbms import postgres as db_utils
 import requests 
 import json, yaml
 from config_parent import Config
+from bs4 import BeautifulSoup
+
 
 logging = log.getLogger()
 logging.setLevel(log.ERROR)
@@ -31,35 +33,35 @@ class Test_api_fetch(unittest.TestCase,Config):
     search_url="""https://www.fpds.gov/ezsearch/search.do?q"""
     search_pg1="https://www.fpds.gov/common/jsp/DocumentSearch.jsp?advancedResultsPage=Y&searchType=advanced&requestFromMainPage=N&pageNumber=1&ContractSelectionType=%20A&ContractSelectionType=A&actionSource=searchCriteriaPage&user=&agency_code=&award_piid=&vendor=GREENZONE+SOLUTIONS%2C+INC.&naics_id=&officeID=&officeName=&idvdAgencyAward_id=&signedFromDate=01/01/2018&signedToDate=12/31/2018&modifiedFromDate=&modifiedToDate=&actionCode=&fundingOfficeID=&fundingAgencyID=&obligatedAmountFrom=&obligatedAmountTo=&MINAMOUNT=&MAXAMOUNT=&modNumber=&STATUS=F&undefined=undefined"
     test_session_data="bbbbb"
-    
+    page_1=os.path.join(Config.dirs['working_dir'],'page_1')
     print(test_session_data)
     def test_1fetch_meta_api(self):
         #pull on once so we don't have to keep pulling
         if not os.path.exists(os.path.join(self.dirs['working_dir'],'page_1')):
+            os.makedirs( self.dirs['working_dir'],exist_ok=True)
             pay_load={self.user_id_field:os.environ["FPDS_USERID"],self.pwd_field:os.environ["FPDS_PWD"]}
             with requests.Session() as session:
                 _ = session.post(self.login_url,data=pay_load)
                 r = session.get(self.search_pg1)  
             #print(self.test_session_data)
-                with open(os.path.join(self.dirs['working_dir'],'page_1')) as f:
+                with open(os.path.join(self.dirs['working_dir'],'page_1'),"w") as f:
                     f.write(r.text)
+                    
+        else:
+            print("Previous Seach was written to file, No Fetch Needed")
              
-            print("test1")
     def test_2process_session_data(self):
-        print(self.test_session_data)
-        print("test2")
-        #test="""https://www.fpds.gov/ezsearch/search.do?q=greenzone+CONTRACTING_AGENCY_NAME%3A%22BUREAU+OF+THE+FISCAL+SERVICE%22+VENDOR_FULL_NAME%3A%22GREENZONE+SOLUTIONS%2C+INC.%22&s=FPDS.GOV&templateName=1.5.1&indexName=awardfull"""
-        
-        
-        
-        
-        
-        #r=requests.post(self.login_url,data=pay_load)
-
-        
-        # print( (r.text))
-        # print( (r.json()))
-        # print(yaml.dump(r.json()))
+        next_search=None
+        with open(self.page_1,"r") as f:
+            soup = BeautifulSoup(f, 'html.parser')
+            priceprice_box = soup.find('href')
+            print(priceprice_box)
+            for a in soup.find_all('a', href=True):
+                if ('/common/jsp/DocumentSearch.jsp?advancedResultsPage') in a['href']:
+                    next_search= a['href']
+        self.assertTrue(next_search is not None)
+         
+           
 
     def test_fetch_api(self):
         print("abc")
