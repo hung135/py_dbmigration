dfaf255f1029806d28f170f32e987a9c54ffc881
Hung Nguyen <hung.nguyen@cfpb.gov>
2019-06-18 14:26:42 +0000

    getting rid of finished processing view

--- src/py_dbmigration/custom_logic/purge_temp_file.py
+++ src/py_dbmigration/custom_logic/purge_temp_file.py
@@ -19,8 +19,15 @@ import os, logging
     Descripton:
     # temporary
 '''
+vw_finished_import="""SELECT DISTINCT p.id,
+    p.file_name_data,
+    p.file_name,
+    c.last_error_msg
+   FROM logging.meta_source_files p
+     LEFT JOIN logging.meta_source_files c ON c.parent_file_id = p.id AND c.file_process_state::text <> 'PROCESSED'::text
+  WHERE p.parent_file_id = 0 AND p.file_process_state::text = 'PROCESSED'::text AND c.id IS NULL"""
 
-check_finish_sql ="select p.id,p.file_name,c.file_path from logging.meta_source_files c, logging.finished_imports p where c.parent_file_id=p.id and c.id={}"
+check_finish_sql ="select p.id,p.file_name,c.file_path from logging.meta_source_files c, ({}) p where c.parent_file_id=p.id and c.id={}"
  
 
 
@@ -31,7 +38,7 @@ def custom_logic(db, foi, df):
         
         if not 's3://' in df.source_file_path:
 
-            parent_file_id,x=db.query(check_finish_sql.format(df.file_id))
+            parent_file_id,x=db.query(check_finish_sql.format(vw_finished_import,df.file_id))
         #print(parent_file_id)
             for id,file_name,file_path in parent_file_id:
                     delete_path=file_path.split(file_name)
