19a2658d767c592db66c15ca2463d5953e35ccac
Hung Nguyen <hung.nguyen@cfpb.gov>
2021-04-02 11:14:41 -0400

    checking in update

---
+++ src/py_dbmigration/sql_runner.py
@@ -0,0 +1,129 @@
+import pandas as pd
+import logging
+from sqlalchemy import create_engine 
+import os 
+import sys 
+import argparse
+from datetime import datetime, timedelta
+ 
+
+
+PGHOST = os.environ['PGHOST']
+PGPORT = os.environ['PGPORT']
+PGDATABASE = os.environ['PGDATABASE']
+PGUSER = os.environ['PGUSER']
+PGPASSWORD = os.environ['PGPASSWORD']
+#a dict to make availe for at runtime for sql queries
+today = datetime.today()
+yesterday = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+ 
+runtime_params={'{today}': today.strftime("%Y%m%d"), 
+                '{yesterday}': yesterday.strftime("%Y%m%d"),
+                '{tomorrow}': tomorrow.strftime("%Y%m%d")
+                }
+
+print(runtime_params)
+def number_to_date(number):
+    date = datetime(year=int(number[0:4]), month=int(number[4:6]), day=int(number[6:8]))
+    return date
+def get_engine():
+    engine = create_engine('postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+        PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE), client_encoding='utf8')
+
+    #print('postgresql://{0}:{1}{2}:{3}/{4}'.format(PGUSER,"*******" , PGHOST, PGPORT, PGDATABASE))
+    return engine
+ 
+ 
+def sql_execute(conn,sql):
+    conn.execute(sql)
+ 
+#stupid function to check for 1 record
+def check_loaded(conn,sql):
+    df=pd.read_sql_query(sql,conn)
+    if len(df)>0:
+        return True
+    return False
+def read_yaml(file_path):
+    import yaml
+
+    with open(file_path) as file:
+        # The FullLoader parameter handles the conversion from YAML
+        # scalar values to Python the dictionary format
+        return yaml.load(file, Loader=yaml.FullLoader)
+
+def apply_runtime_value(sql,runtime_params):
+    return_sql=sql
+    for key,v in runtime_params.items():        
+        return_sql=return_sql.replace(key,v)
+
+    return return_sql
+def parse_sql(dbconnection,yaml,runtime_params):
+    for key in yaml:
+        logging.debug(f"RUNNING : {key}")
+        yaml_content=yaml[key]
+        check_if_exists = yaml_content.get('check_if_exists',None)
+        
+        sql_list = yaml_content.get('sqls',None)
+        data_already_exists=False
+        
+        if check_if_exists is not None:
+            logging.debug("Checking Data existance")
+            
+            data_already_exists=check_loaded(dbconnection,apply_runtime_value(check_if_exists,runtime_params))
+        
+        if not data_already_exists:
+            for sql in sql_list:
+                runtime_sql=apply_runtime_value(sql,runtime_params)
+                logging.debug(f"Running SQL\n****************************\n{runtime_sql}\n****************************")
+                #sql_execute(dbconnection,runtime_sql)
+        else:
+            logging.debug("Data exists aborting")
+            #sql_execute(conn,sql)
+        
+
+def main(args):
+    
+    dbconnection=get_engine() 
+    start_date=number_to_date(args.start)
+    end_date=number_to_date(args.end)
+    
+    print(start_date)
+    yaml_sql=read_yaml(args.file)
+    
+    days=pd.date_range(start_date,end_date-timedelta(days=1),freq='d')
+    for day in days:
+        s = day.strftime("%Y%m%d")
+        runtime_params['{day}']=s
+        parse_sql(dbconnection,yaml_sql,runtime_params)
+
+ 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", 
+            help="Increase output (Debug Log Level)", action="store_true")
+    parser.add_argument('-f','--file',     
+            help=f'Defaults to ./break_out.yaml', default='break_out.yaml')
+    parser.add_argument('-s','--start',     
+            help=f'Start date ({today.strftime("%Y%m%d")})', default=today.strftime("%Y%m%d"))
+    parser.add_argument('-e', '--end',   
+            help=f'End date ({today.strftime("%Y%m%d")})',default=today.strftime("%Y%m%d"))
+    
+
+    args = parser.parse_args()
+     
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+     
+    args = parser.parse_args()
+
+    start_time = datetime.now()
+    logging.info('start_time %s', start_time)
+    exit = main(args)
+    end_time = datetime.now()
+    elapsed_time = end_time - start_time
+    logging.info('end_time %s', end_time)
+    logging.info('elapsed_time %s', elapsed_time)
+    sys.exit(exit)
--- src/py_dbmigration/zip_utils/README.md
+++ src/py_dbmigration/zip_utils/README.md
@@ -1,1 +1,89 @@
-This will be where documentation begins
+# PY_DBMIGRATION
+
+This is a python framework that will facility the loading of various data files into a postgres database.  The framework will do various things for the user:
+```
+Inventory raw files
+Manage file state
+Provides ability to write custom ingestion logic
+Allows parrallel ingestion
+Allows for multi-core ingestion
+Writes to logging data table
+Allows for reprocessing of failed filed
+Yaml driven configuration
+Pattern file matching via REGEX
+```
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+
+
+## Dev Environment
+
+* [vscode] (https://code.visualstudio.com/) - Development IDE
+    ```brew cask install vscode```
+* [Docker](https://www.docker.com/get-started/) - Docker Containerization
+* [Remote-dev extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.vscode-remote-extensionpack) - Vscode extension for development inside docker container
+ 
+### Prerequisites
+
+What things you need to install the software and how to install them
+
+```
+vscode
+docker
+remote-dev extension
+```
+
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+vscode <project folder name>
+open project in container
+```
+
+And repeat
+
+```
+until finished
+```
+
+End with an example of getting some data out of the system or using it for a little demo
+
+## Running the tests
+
+Explain how to run the automated tests for this system
+
+### Break down into end to end tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+### And coding style tests
+
+Explain what these tests test and why
+
+```
+Give an example
+```
+
+## Deployment
+
+Add additional notes about how to deploy this on a live system
+
+## Built With
+
+* [Dropwizard](http://www.dropwizard.io/1.0.2/docs/) - The web framework used
+* [Maven](https://maven.apache.org/) - Dependency Management
+* [ROME](https://rometools.github.io/rome/) - Used to generate RSS Feeds
+ 
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+ 
