d73c23131683c3dfed343f6bae04e37ba771e281
Hung Nguyen <hung.nguyen@cfpb.gov>
2021-05-27 10:29:20 -0400

    updating upsert custom logic

--- src/py_dbmigration/custom_logic/upsert_table_from_stg.py
+++ src/py_dbmigration/custom_logic/upsert_table_from_stg.py
@@ -1,14 +1,14 @@
 
- 
+import os
+import logging as lg
 import sys
-import py_dbutils.rdbms.postgres as db_utils
-import py_dbmigration.data_file_mgnt as data_file_mgnt
-import py_dbmigration.migrate_utils as migrate_utils
-from py_dbmigration.data_file_mgnt.state import FOI, LogicState
+from py_dbutils.rdbms.postgres import DB
+from py_dbmigration.data_file_mgnt.data_files import DataFile  
+from py_dbmigration.data_file_mgnt.state import LogicState, FOI
+import py_dbmigration.migrate_utils as migrate_utils 
  
-import os, logging as lg
 
-logging=lg.getLogger()
+logging=lg.getLogger() 
 
 
 '''
@@ -28,13 +28,22 @@ logging=lg.getLogger()
 # update_sql = """UPDATE logging.meta_source_files set  crc='{}'  where id = {}"""
 
 
-def custom_logic(db, foi, df,logic_status):
+def custom_logic(db: DB, foi: FOI, df: DataFile,logic_status: LogicState):
     continue_processing = True
     table_name = foi.table_name
     data_schema = db.dbschema
     stage_schema = 'stg'
     primary_key = db.get_primary_keys(data_schema + "." + table_name)
     update_sql = None
+    if not isinstance(foi.CURRENT_LOGIC_CONFIG,str):
+        data_schema=foi.CURRENT_LOGIC_CONFIG.get('data_schema',data_schema)
+        stage_schema=foi.CURRENT_LOGIC_CONFIG.get('stage_schema',stage_schema)
+        delim=foi.CURRENT_LOGIC_CONFIG.get('delimiter',delim) 
+        table_name=foi.CURRENT_LOGIC_CONFIG.get('table_name',table_name)
+        encoding=foi.CURRENT_LOGIC_CONFIG.get('encoding',encoding)
+        config_column_list=foi.CURRENT_LOGIC_CONFIG.get('column_list',config_column_list)
+        encoding=foi.CURRENT_LOGIC_CONFIG.get('file_encoding',encoding)
+
 
     if len(primary_key) == 0:
         no_upsert_sql = """insert into {target_schema}.{table_name} ({column_list}) select {column_list} from {src_schema}.{table_name}"""
@@ -50,23 +59,21 @@ def custom_logic(db, foi, df,logic_status):
         update_sql = upsert_sql
         logging.info("\t\tPrimary Found Loading via Upsert: {}".format(primary_key))
 
-    logging.info("\t\tSetting file_id to: {}".format(df.file_id))
-    cnt = db.execute("update stg.{} set file_id={}".format(table_name, df.file_id),catch_exception=False)
-    logging.info("\t\tLoading...could take a while: {} Records:".format(cnt))
+    #logging.info("\t\tSetting file_id to: {}".format(df.file_id))
+    #cnt = db.execute("update stg.{} set file_id={}".format(table_name, df.file_id),catch_exception=False)
+    logging.info("\t\tLoading...could take a while:")
     logging.debug(update_sql)
     x = db.execute(update_sql,catch_exception=False)
-    logging.info("\t\tUpsert compleated: {}".format(x))
+    logging.debug("\t\tUpsert completed: {}".format(x))
     # def custom_logic(db, schema, table_name, column_list=None, where_clause='1=1'):
 
     return continue_processing
 # Generic code...put your custom logic above to leave room for logging activities and error handling here if any
 
 
-def process(db, foi, df):
-    error_msg = None
-    additional_msg = None
-
+def process(db, foi, df, logic_status):
+    
     assert isinstance(foi,FOI)
-    assert isinstance(db, db_utils.DB)
-    assert isinstance(logic_status,LogicState)
-    return custom_logic(db, foi, df,logic_status)
+    assert isinstance(db, DB)
+    assert isinstance(logic_status, LogicState)
+    return custom_logic(db, foi, df, logic_status)
