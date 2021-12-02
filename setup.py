933a018b7b4a6707d6c88680e95e92b82dc9176e
Hung Nguyen <hung.nguyen@cfpb.gov>
2021-12-02 17:49:48 -0500

    adding required dependencies

--- setup.py
+++ setup.py
@@ -13,14 +13,11 @@ import sys
 from pkg_resources import require, VersionConflict
 from setuptools import setup
 
-# try:
-#     require('setuptools>=38.3')
-#     require('py-dbutils')
-#     require('pyyaml')
-#     require('xlrd')
-# except VersionConflict:
-#     print("Error: version of setuptools is too old (<38.3)!")
-#     sys.exit(1)
+try:
+    require('setuptools>=38.3')
+except VersionConflict:
+    print("Error: version of setuptools is too old (<38.3)!")
+    sys.exit(1)
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
